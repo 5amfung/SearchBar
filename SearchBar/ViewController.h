615e6487999a1a4af40e88e498394e0dd6a5c623
Sam Fung <sam.fung@gmail.com>
2014-02-23 14:21:56 -0800

    Initial Commit

---
+++ SearchBar/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SearchBar
+//
+//  Created by bbpan on 2/23/14.
+//  Copyright (c) 2014 sfng.co. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
